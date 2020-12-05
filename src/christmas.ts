a8874e9c1a6be1d54da76f391c1d9b1edf765fee
Nathan Kellenicki <nathan@kellenicki.com>
2020-12-05 15:58:43 -0800

    Christmas train web server

--- src/christmas.ts
+++ src/christmas.ts
@@ -1,20 +1,26 @@
 import WeDo from "node-wedo";
+import express from "express";
 
 import WeDoMotionSensor from "./wedomotionsensor";
 import WeDoTrain from "./wedotrain";
+import { getHeapCodeStatistics } from "v8";
+
 const weDo = new WeDo();
+const app = express();
 
 const delay = (milliseconds = 500) => new Promise((resolve) => setTimeout(resolve, milliseconds));
 
 enum State {
     LOOPING = 0,
-    STOPPING = 1,
-    STOPPED = 2,
-    STARTING = 3
+    SLOWING = 1,
+    PAUSED = 2,
+    STOPPED = 3,
+    STARTING = 4
 }
 
 let state = State.STOPPED;
 let loop = 0;
+let wantStop = true;
 
 (async () => {
 
@@ -22,19 +28,22 @@ let loop = 0;
     await weDo.connect();
     console.log("WeDo connected");
 
-    const stationSensor = new WeDoMotionSensor(weDo, "StationSensor", "B");
+    const stationSensor = new WeDoMotionSensor(weDo, "StationSensor", "B", 180);
     const christmasTrain = new WeDoTrain(weDo, "ChristmasTrain", "A");
 
     const stopTrain = async () => {
-        state = State.STOPPING;
+        console.log("Slowing Christmas Train");
+        state = State.SLOWING;
         for (let i = 7; i >= 0; i--) {
             christmasTrain.go(i);
             await delay(500);
         }
-        state = State.STOPPED;
+        state = State.PAUSED;
+        console.log("Paused Christmas Train");
     };
 
     const startTrain = async () => {
+        console.log("Starting Christmas Train");
         state = State.STARTING;
         for (let i = 1; i <= 8; i++) {
             christmasTrain.go(i);
@@ -44,24 +53,52 @@ let loop = 0;
         loop = 0;
     };
 
-    await delay(2000);
-    console.log("Starting Christmas Train");
-    await startTrain();
-
     stationSensor.on("detected", async (sensorState: number) => {
         console.log(`Sensor ${sensorState}`);
         if (sensorState === 0) {
             if (state === State.LOOPING && loop === 2) {
-                console.log("Stopping Christmas Train");
                 await stopTrain();
                 await delay(10000);
-                console.log("Starting Christmas Train");
-                await startTrain();
+                if (wantStop) {
+                    state = State.STOPPED;
+                    console.log("Stopped Christmas Train");
+                    return;
+                } else {
+                    await startTrain();
+                }
             } else if (state === State.LOOPING) {
                 console.log(`Loop ${loop}`);
                 loop++;
             }
         }
+    });
+
+    app.get("/christmastrain/stop", (_, res) => {
+        console.log("Received Christmas Train stop request");
+        wantStop = true;
+        res.send("ok");
+    });
+    
+    app.get("/christmastrain/start", (_, res) => {
+        console.log("Received Christmas Train start request");
+        wantStop = false;
+        if (state === State.STOPPED) {
+            startTrain();
+        }
+        res.send("ok");
+    });
+
+    app.get("/christmastrain/status", (_, res) => {
+        res.send(String(wantStop ? 0 : 1));
+    });
+
+    app.use((_, res, next) => {
+        res.set("Cache-Control", "no-store");
+        next()
+    });
+
+    app.listen(3000, () => {
+        console.log("Christmas Train listening on port 3000");
     });
 
 })();
--- src/wedomotionsensor.ts
+++ src/wedomotionsensor.ts
@@ -14,19 +14,21 @@ export default class WeDoMotionSensor extends EventEmitter {
     private _name: string;
     private _state: State = State.NOT_DETECTED;
     private _isWaiting: boolean = false;
+    private _distance: number = 80;
 
     private _timer: NodeJS.Timer | undefined;
 
 
-    constructor (weDo: WeDo, name: string, port: string) {
+    constructor (weDo: WeDo, name: string, port: string, distance: number = 80) {
         super();
         this._weDo = weDo;
         this._name = name;
+        this._distance = distance;
         this.port = port;
         this._weDo.on("distance", (port, { distance }) => {
             // console.log(this._name, distance);
             if (port === this.port) {
-                if (distance < 80) {
+                if (distance < this._distance) {
                     // console.log(this._name, distance);
                     this._isWaiting = false;
                     if (this._timer) {
