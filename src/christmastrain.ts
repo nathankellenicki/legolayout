d4eec13634d6291e2d109b0a63c5d4d282d3c82c
Nathan Kellenicki <nathan@kellenicki.com>
2020-12-17 17:17:05 -0800

    Adjusted speed

--- src/christmastrain.ts
+++ src/christmastrain.ts
@@ -64,7 +64,6 @@ let wantStop = true;
         console.log(`Sensor ${sensorState}`);
         if (sensorState === 0) {
             if (state === State.LOOPING && loop >= 2) {
-                await delay(500);
                 await stopTrain();
                 await delay(10000);
                 if (wantStop) {
